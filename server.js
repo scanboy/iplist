ea80accca5a208941c07b7519c4b332f36087679
klau <klau1999@yahoo.com>
2017-10-12 13:28:20 -0700

    Test v2.0

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.multipart());
+
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
