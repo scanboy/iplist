faf33e9c8a5cd9bfb001ac3dd9dce928f006fc3b
klau <klau1999@yahoo.com>
2017-10-17 08:03:58 -0700

    Test v2.4

--- server.js
+++ server.js
@@ -145,25 +145,6 @@ app.get('/updateip', function(req, res) {
   fs.writeFileSync(path + 'iplist.json', JSON.stringify(iplist));
 });
 
-// app.get('/', function (req, res) {
-//   // try to initialize the db on every request if it's not already
-//   // initialized.
-//   // if (!db) {
-//   //   initDb(function(err){});
-//   // }
-//   // if (db) {
-//   //   var col = db.collection('counts');
-//   //   // Create a document with request IP and current time of request
-//   //   col.insert({ip: req.ip, date: Date.now()});
-//   //   col.count(function(err, count){
-//   //     res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-//   //   });
-//   // } else {
-//   //   res.render('index.html', { pageCountMessage : null});
-//   // }
-//   res.send('<html><head></head><body><h1>welcome, klau!</h1></body></html>')
-// });
-
 // app.get('/pagecount', function (req, res) {
 //   // try to initialize the db on every request if it's not already
 //   // initialized.
